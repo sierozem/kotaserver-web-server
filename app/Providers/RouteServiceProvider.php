ca836de9ae711f971faa98af4c724fec86398f80
sierozem <sierozem0@yahoo.co.jp>
2021-12-14 00:56:02 +0900

    既存の静的解析エラーを無視するよう設定

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -57,6 +57,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting()
     {
         RateLimiter::for('api', function (Request $request) {
+            /** @phpstan-ignore-next-line */
             return Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip());
         });
     }
